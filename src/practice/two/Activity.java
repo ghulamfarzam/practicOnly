24bf88ae9ab92f25ab57bdf59a4fb266c9051c9e
farzamali <gh.alifarzam@gmail.com>
2023-06-05 22:29:45 -0400

    new

--- src/practice/two/Activity.java
+++ src/practice/two/Activity.java
@@ -2,10 +2,23 @@ package practice.two;
 
 public class Activity {
 
-	public static void main(String[] args) {
+		public static String getEmail() {
+			String email = "farzam";
+			String providor = "@gmail.com";
+			int randomNumber = (int) (Math.random() * 100);
+			String autoEmail = email + randomNumber + providor;
+			return autoEmail;
+		}
+		
+		
+
+		public static void main(String[] args) {
 		System.out.println("Hello world this is the github repository");
 		System.out.println("this is the second github");
-
+		System.out.println(getEmail());
 	}
+	
+		
+	
 
 }
