e0516f835d67f596b8d54c0d6ed7af2d31004b87
farzamali <gh.alifarzam@gmail.com>
2023-06-05 22:33:35 -0400

    new

--- src/practice/two/Activity.java
+++ src/practice/two/Activity.java
@@ -10,12 +10,21 @@ public class Activity {
 			return autoEmail;
 		}
 		
+		public static String getPhoneNumber() {
+			String phoneNumber = "9";
+			for(int i = 0; i < 9; i++) {
+				phoneNumber += (int) (Math.random() * 10);
+			}
+			return phoneNumber;
+		}
+		
 		
 
 		public static void main(String[] args) {
 		System.out.println("Hello world this is the github repository");
 		System.out.println("this is the second github");
 		System.out.println(getEmail());
+		System.out.println(getPhoneNumber());
 	}
 	
 		
